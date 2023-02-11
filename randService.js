beec2e5a15a2c947e50f512e44ad3e3e8e439c1b
PisethC92 <91227009+PisethC92@users.noreply.github.com>
2023-02-11 10:18:57 -0500

    Removed testing data

--- randService.js
+++ randService.js
@@ -7,10 +7,6 @@ const app = express();
 // Use the body-parser middleware to parse JSON data in the request body
 app.use(bodyParser.json());
 
-const data = [
-    {'id': 1630173, 'full_name': 'Precious Achiuwa', 'first_name': 'Precious', 'last_name': 'Achiuwa', 'is_active': true},
-    {'id': 203500, 'full_name': 'Steven Adams', 'first_name': 'Steven', 'last_name': 'Adams', 'is_active': true}
-  ];
 
 // Define an endpoint for the POST request
 app.post('/random-select', (req, res) => {
